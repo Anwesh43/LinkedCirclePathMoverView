5f61b0c8b70d9b154504e4a64f414f6a753602e6
Anwesh43 <anweshthecool0@gmail.com>
2018-08-15 01:49:13 +0530

    importing libraries and defining nodes

---
+++ circlepathmoverview/src/main/java/com/anwesh/uiprojects/circlepathmoverview/CirclePathMoverView.kt
@@ -0,0 +1,14 @@
+package com.anwesh.uiprojects.circlepathmoverview
+
+/**
+ * Created by anweshmishra on 15/08/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.content.Context
+
+val nodes : Int = 5
