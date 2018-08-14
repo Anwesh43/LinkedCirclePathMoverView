9b2f2ae76c9a3974d2f9c47d736b067eb78ac65b
Anwesh43 <anweshthecool0@gmail.com>
2018-08-15 02:13:08 +0530

    adding canvas extension method drawCirclePathNode

--- circlepathmoverview/src/main/java/com/anwesh/uiprojects/circlepathmoverview/CirclePathMoverView.kt
+++ circlepathmoverview/src/main/java/com/anwesh/uiprojects/circlepathmoverview/CirclePathMoverView.kt
@@ -8,10 +8,28 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
-import android.graphics.Path
+import android.graphics.RectF
 import android.content.Context
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawCirclePathNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / nodes
+    val r : Float = gap / (2 * Math. PI).toFloat()
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.color = Color.parseColor("#4CAF50")
+    save()
+    translate(gap * i + gap / 2, h / 2)
+    drawArc(RectF(-r, -r, r, r), 360f * sc1, 360f * (1 - sc1), false, paint)
+    drawLine(r + gap * sc2, 0f, r + gap * sc1, 0f, paint)
+    restore()
+}
 
 class CirclePathMoverView(ctx : Context) : View(ctx) {
 
