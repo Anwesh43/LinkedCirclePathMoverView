b94ea5a256e101eb62f22ebdb64ade32eff1c5fa
Anwesh43 <anweshthecool0@gmail.com>
2018-08-15 14:05:15 +0530

    adding AnimationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcirclepathmoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcirclepathmoverview/MainActivity.kt
@@ -3,14 +3,21 @@ package com.anwesh.uiprojects.linkedcirclepathmoverview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.circlepathmoverview.CirclePathMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CirclePathMoverView.create(this)
+        val view : CirclePathMoverView = CirclePathMoverView.create(this)
         fullScreen()
+        view.addAnimationCompletionListener({createToast("animation ${it + 1} completed")}, {createToast("animation ${it + 1} is reset")})
+
+    }
+
+    fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
