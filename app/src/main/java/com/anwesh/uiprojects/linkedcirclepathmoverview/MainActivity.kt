ebbc7ef84df937ee27f358532a6be0f0ce05fa51
Anwesh43 <anweshthecool0@gmail.com>
2018-08-15 13:28:42 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcirclepathmoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcirclepathmoverview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedcirclepathmoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circlepathmoverview.CirclePathMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        CirclePathMoverView.create(this)
     }
 }
